bdfdf839c33f6b35de60032ed5e46fb040aca831
thisMagpie <thisMagpie@users.noreply.github.com>
2016-02-19 13:16:56 +0000

    add save function

--- cern_a.py
+++ cern_a.py
@@ -9,4 +9,6 @@ max = 11
 pylab.hist(xmass, bins = N , range=[min, max])
 pylab.xlabel('M (GeV/c^2)')
 pylab.ylabel('Candidates /(MeV/c^2)')
+pylab.title('Candidates /(MeV/c^2) vs M (GeV/c^2)')
+pylab.savefig('cern.png')
 pylab.show()
