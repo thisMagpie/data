3d567af24f35dbf1845c6262c0256d87bb44ce3e
thisMagpie <thisMagpie@users.noreply.github.com>
2016-02-19 12:02:13 +0000

    Update cern_a.py

--- cern_a.py
+++ cern_a.py
@@ -1,8 +1,8 @@
 import pylab
 import numpy as np
 
-xmass = np.genfromtxt('upsilons-mass-pt-xaa.txt', delimiter=' ', dtype=None)
-N = 330
+xmass = np.genfromtxt('upsilons-mass-xaa.txt', delimiter=' ', dtype=None)
+N = 332
 min = 8.5
 max = 11
 
